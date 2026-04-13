971b8460bf4cf54f8bd7f9d86dea42f76fc0f3da
KunoAiru <airu.foruri39@gmail.com>
2026-04-13 13:56:02 +0900

    初回コミット

---
+++ src/preprocess.py
@@ -0,0 +1,17 @@
+import sklearn
+import matplotlib
+import optuna
+import torch
+import wandb
+import seaborn
+import transformers
+import tqdm
+
+print(sklearn.__version__)
+print(matplotlib.__version__)
+print(optuna.__version__)
+print(torch.__version__)
+print(wandb.__version__)
+print(seaborn.__version__)
+print(transformers.__version__)
+print(tqdm.__version__)
